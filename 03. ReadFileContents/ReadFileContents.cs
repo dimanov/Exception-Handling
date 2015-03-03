2bc2df7c22b7514e871ad2dfe593d6797c88911b
dimanov <dimanov@gmail.com>
2015-03-03 15:59:13 +0200

    SquareRoot Solved

---
+++ 03. ReadFileContents/ReadFileContents.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Problem 3. Read file contents
+Write a program that enters file name along with its full file path (e.g. C:\WINDOWS\win.ini), reads its contents and prints it on the console.
+Find in MSDN how to use System.IO.File.ReadAllText(…).
+Be sure to catch all possible exceptions and print user-friendly error messages.
+ */
+
+using System;
+using System.IO;
+
+class ReadFileContents
+{
+    static void Main()
+    {
+        //Console.WriteLine("Please enter the full path of the file:");
+        //string filePath = Console.ReadLine();
+        string filePath = @"C:\WINDOWS\win.ini";
+        ReadFile(filePath);
+    }
+
+    private static void ReadFile(string filePath)
+    {
+        TextReader reader = new StreamReader(filePath);
+        try
+        {
+            string line = reader.ReadLine();
+            Console.WriteLine(line);
+        }
+        finally
+        {
+            reader.Close();
+        }
+    }
+}
